e01b71d8fbcd06eff828167c9555868eb24431b1
Eduardo <ibredu@gmail.com>
2014-08-13 23:10:44 -0500

    Added new fields and association to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813014351) do
+ActiveRecord::Schema.define(version: 20140814040348) do
 
   create_table "candidate_votations", force: true do |t|
     t.integer  "candidate_id"
@@ -117,6 +117,10 @@ ActiveRecord::Schema.define(version: 20140813014351) do
     t.string   "cellphone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "rnm"
+    t.integer  "parent"
+    t.string   "section"
+    t.integer  "zipcode"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
