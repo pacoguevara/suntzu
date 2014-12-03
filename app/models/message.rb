48499285bc2755010cf9d60725ad9cd24a8f31ae
Paco Guevara <pacoguevaraa@gmail.com>
2014-12-03 09:23:28 -0600

    Se habilito el uso de twilio para el envio de sms

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,31 +1,31 @@
 class Message < ActiveRecord::Base
   belongs_to :user_message
+  @twilio_sid = "ACed7a7ef98a126abc33ff867370d890c9"
+  @twilio_token = "6e26775044f3087e7d9f44f003db29ae"
+  @twilio_phone_number = "+18027339326"
 
   def self.send_sms(to, message, user_id, message_id)
-  	twilio_sid = "ACed7a7ef98a126abc33ff867370d890c9"
-    twilio_token = "6e26775044f3087e7d9f44f003db29ae"
-    twilio_phone_number = "+18027339326"
-    number_to_send_to = "+528341444418"
-    #@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-    #response = @twilio_client.account.sms.messages.create(
-    #  :from => "#{twilio_phone_number}",
-    #  :to => "+52"+to,
-    #  :body => message
-    #)
+  	number_to_send_to = "+528341444418"
+    @twilio_client = Twilio::REST::Client.new @twilio_sid, @twilio_token
+    response = @twilio_client.account.sms.messages.create(
+      :from => "#{@twilio_phone_number}",
+      :to => "+52"+to,
+      :body => message
+    )
     #puts response.status
     #puts response.sid
-    #mid = response.sid
-    mid = '2e3r4t'
+    mid = response.sid
+    #mid = '2e3r4t'
     return UserMessage.create user_id, message_id, mid
 
   end
 
   def self.get_sms(message_sid)
-    twilio_sid = "ACed7a7ef98a126abc33ff867370d890c9"
-    twilio_token = "6e26775044f3087e7d9f44f003db29ae"
-    twilio_phone_number = "+18027339326"
+    #twilio_sid = "ACed7a7ef98a126abc33ff867370d890c9"
+    #twilio_token = "6e26775044f3087e7d9f44f003db29ae"
+    #twilio_phone_number = "+18027339326"
     begin
-      @client = Twilio::REST::Client.new twilio_sid, twilio_token
+      @client = Twilio::REST::Client.new @twilio_sid, @twilio_token
       message = @client.account.messages.get(message_sid)
       return message.status
     rescue Twilio::REST::RequestError => e
