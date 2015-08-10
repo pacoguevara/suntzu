339806334f473abb238ef708eec4871d1a3035fb
Paco Guevara <pacoguevaraa@gmail.com>
2015-08-10 11:42:16 -0500

    last row

--- app/models/user.rb
+++ app/models/user.rb
@@ -421,7 +421,7 @@ class User < ActiveRecord::Base
     puts enlaces
     puts subenlaces
 
-    (2..10).each do |i|
+    (2..spreadsheet.last_row).each do |i|
       folio = spreadsheet.cell(i,1).to_i.to_s 
       first_name = spreadsheet.cell(i,2)
       last_name = spreadsheet.cell(i,3)
